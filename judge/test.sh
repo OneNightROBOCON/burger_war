56c535dcbe66cf00a3c1f283fb79fd8505f54bb3
yupos0221 <katorapa21@gmail.com>
2017-09-29 18:24:46 +0900

    add output window for showing state

--- judge/test.sh
+++ judge/test.sh
@@ -2,9 +2,13 @@
 echo "=================RESET======================"
 curl localhost:5000/reset
 
+sleep 1
+
 # GET war state initial
 echo "=================STATE initial======================"
 curl localhost:5000/warState
+
+sleep 1
 
 # regist targets 
 echo "=================REGIST targets======================"
@@ -12,23 +16,33 @@ curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"two", "id":"2222", "point":3}' localhost:5000/warState/targets
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"three", "id":"3333", "point":3}' localhost:5000/warState/targets
 
+sleep 1
+
 # regist players
 echo "=================REGIST players======================"
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro"}' localhost:5000/warState/players
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"jiro"}' localhost:5000/warState/players
 
+sleep 1
 
 # ready
 echo "=================set ready players======================"
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro", "side":"r", "id":"0000"}' localhost:5000/submits
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"jiro", "side":"b", "id":"0000"}' localhost:5000/submits
 
+sleep 1
+
 # set state to "running"
 echo "=================set state "running"======================"
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state":"running"}' localhost:5000/warState/state
 
+sleep 1
+
 echo "=================STATE running======================"
 curl localhost:5000/warState
+
+sleep 1
+
 
 # submit
 echo "=================submit target id======================"
@@ -38,9 +52,16 @@ curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"jiro", "side":"b", "id":"2222"}' localhost:5000/submits
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"name":"ishiro", "side":"r", "id":"2223"}' localhost:5000/submits
 
+sleep 1
+
+
 # set state to "end"
 echo "=================set state "end"======================"
 curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"state":"stop"}' localhost:5000/warState/state
 
+sleep 1
+
+
 echo "=================STATE end======================"
 curl localhost:5000/warState
+
