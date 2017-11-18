7e9f18c3c5320ab81f625a9895658c311243b520
dashimaki360 <17.punch@gmail.com>
2017-11-18 15:53:01 +0900

    change dummy id

--- scripts/dummyArReader.py
+++ scripts/dummyArReader.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     rospy.init_node("qr_reader")
     # publish qr_val
     qr_val_pub = rospy.Publisher('qr_val', String, queue_size=10)
-    sample_id_list = ['hoge', 'fooo', 'barr', 'code',
-                      'onig', 'robo', 'seve', 'byte']
+    sample_id_list = ['0001', '0002', '0003', '0004',
+                      '0005', '0006', '0007', '0008']
     while not rospy.is_shutdown():
         for qr_id in sample_id_list:
             qr_val_pub.publish(qr_id)
