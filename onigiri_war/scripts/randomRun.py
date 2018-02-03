36219a69b64014c32711cbf279bb609c909496b4
dashimaki360 <17.punch@gmail.com>
2018-02-04 02:23:10 +0900

    add randomRun.py

---
+++ onigiri_war/scripts/randomRun.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import rospy
+import random
+
+from geometry_msgs.msg import Twist
+#from std_msgs.msg import String
+#from sensor_msgs.msg import Image
+#from cv_bridge import CvBridge, CvBridgeError
+#import cv2
+
+
+class RandomBot():
+    def __init__(self, bot_name):
+        # bot name 
+        self.name = bot_name
+        # velocity publisher
+        self.vel_pub = rospy.Publisher('/cmd_vel', Twist,queue_size=1)
+
+    def calcTwist(self):
+        value = random.randint(1,1000)
+        if value < 250:
+            x = 0.2
+            th = 0
+        elif value < 500:
+            x = -0.2
+            th = 0
+        elif value < 750:
+            x = 0
+            th = 1
+        elif value < 1000:
+            x = 0
+            th = -1
+        else:
+            x = 0
+            th = 0
+        twist = Twist()
+        twist.linear.x = x; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th
+        return twist
+
+    def strategy(self):
+        r = rospy.Rate(1) # change speed 1fps
+
+        target_speed = 0
+        target_turn = 0
+        control_speed = 0
+        control_turn = 0
+
+        while not rospy.is_shutdown():
+            twist = self.calcTwist()
+            print(twist)
+            self.vel_pub.publish(twist)
+
+            r.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('random_rulo')
+    bot = RandomBot('Random')
+    bot.strategy()
+
