4c50c3ec58437334285bb71fe143d5fd9519ce18
takaoka-yuki <takaoka.yuki@jp.panasonic.com>
2018-07-18 16:59:42 +0900

    change name random > enemy

--- scripts/sim_with_judge.sh
+++ scripts/sim_with_judge.sh
@@ -8,7 +8,7 @@ gnome-terminal -e "python onigiri_war_judge/judgeServer.py"
 gnome-terminal -e "python onigiri_war_judge/visualizeWindow.py"
 
 # init judge server for sim setting
-bash onigiri_war_judge/test_scripts/init_single_play.sh onigiri_war_judge/marker_set/sim.csv localhost:5000 foo bar
+bash onigiri_war_judge/test_scripts/init_single_play.sh onigiri_war_judge/marker_set/sim.csv localhost:5000 you enemy
 
 # robot
 roslaunch onigiri_war onigiri_setup_sim.launch
